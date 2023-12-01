6130c608cc3cabf7631a5388c1d5b89d3e368903
Vishal <vishaljain7100@gmail.com>
2023-12-01 22:45:50 +0530

    working on image to choose from gallery

---
+++ Schema.js
@@ -0,0 +1,6 @@
+const Joi = require("joi")
+
+
+const post = Joi.object({
+    
+})
--- routes/Admin.js
+++ routes/Admin.js
@@ -2,6 +2,21 @@ const express = require("express")
 const router = express.Router()
 const wrapAsync = require("../utility/wrapAsync")
 const ExpressError = require("../utility/ExpressError")
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cd) {
+        cd("null", "./uploads")
+    },
+    filename: function (req, file, cd) {
+        const suppfix = Date.now() + Math.floor(Math.round() * 10000)
+        cd("null", file.filename + suppfix)
+
+    }
+})
+
+const upload = multer({ storage: storage })
+
 
 router.get("/", wrapAsync(async (req, res, next) => {
     res.render("Admin/Admin.ejs")
@@ -11,6 +26,15 @@ router.get("/", wrapAsync(async (req, res, next) => {
 router.get("/Category", wrapAsync(async (req, res, next) => {
     res.render('Admin/AddCategory.ejs')
 }))
+
+// Add Category (Post route)
+router.post("/Category", upload.single("image"), wrapAsync(async (req, res, next) => {
+    res.render('Admin/AddCategory.ejs')
+    console.log(req.body)
+    console.log(req.file)
+}))
+
+
 
 
 module.exports = router
