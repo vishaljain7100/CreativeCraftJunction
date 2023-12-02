8a1370c2c312b9fd8393d1020c5a7cdab29ff4d7
Vishal <vishaljain7100@gmail.com>
2023-12-02 13:08:28 +0530

    working on multi photo to store it in db :

--- routes/Admin.js
+++ routes/Admin.js
@@ -5,18 +5,16 @@ const ExpressError = require("../utility/ExpressError")
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cd) {
-        cd("null", "./uploads")
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
     },
-    filename: function (req, file, cd) {
-        const suppfix = Date.now() + Math.floor(Math.round() * 10000)
-        cd("null", file.filename + suppfix)
-
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + ".png")
     }
 })
 
 const upload = multer({ storage: storage })
-
 
 router.get("/", wrapAsync(async (req, res, next) => {
     res.render("Admin/Admin.ejs")
@@ -27,14 +25,22 @@ router.get("/Category", wrapAsync(async (req, res, next) => {
     res.render('Admin/AddCategory.ejs')
 }))
 
+
 // Add Category (Post route)
-router.post("/Category", upload.single("image"), wrapAsync(async (req, res, next) => {
-    res.render('Admin/AddCategory.ejs')
-    console.log(req.body)
-    console.log(req.file)
-}))
+router.post("/Category",
+    upload.fields([
+        { name: "listing[image1]", maxCount: 1 },
+        { name: "listing[image2]", maxCount: 1 },
+        { name: "listing[image3]", maxCount: 1 },
+    ]),
+    wrapAsync(async (req, res) => {
+        console.log(req.files["listing[image1]"][0].filename)
+        let image1 = req.files['listing[image1]'][0].filename
+        let image2 = req.files['listing[image2]'][1].filename
+        let image3 = req.files['listing[image3]'][2].filename
 
-
-
+        console.log(image1,image2,image3)
+        res.redirect('/Admin')
+    }))
 
 module.exports = router
